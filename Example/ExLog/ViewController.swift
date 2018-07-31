825364b3e26596fe13d6fbe2fa863678fc5b97fc
Chorokichi <kdy.developer@gmail.com>
2018-07-31 11:11:08 +0900

    Initial commit

---
+++ Example/ExLog/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ExLog
+//
+//  Created by Jirokichi on 07/31/2018.
+//  Copyright (c) 2018 Jirokichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
